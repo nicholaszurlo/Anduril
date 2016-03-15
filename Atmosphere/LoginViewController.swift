35598f14207b0acd6967f55762e5506f46d6fcf6
Evan Scott <evanscott@me.com>
2016-03-14 20:50:42 -0400

    First Commit

---
+++ Atmosphere/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Atmosphere
+//
+//  Created by Evan Scott on 3/13/16.
+//  Copyright © 2016 Evan Scott. All rights reserved.
+//
+
+import Foundation
---
+++ Atmosphere/RegisterViewController.swift
@@ -0,0 +1,9 @@
+//
+//  RegisterViewController.swift
+//  Atmosphere
+//
+//  Created by Evan Scott on 3/14/16.
+//  Copyright © 2016 Evan Scott. All rights reserved.
+//
+
+import Foundation
